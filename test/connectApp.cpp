1b5aed5a538d44b55e64edd6e34d9e3f25390ac9
Greg Zaitsev <greg.zaitsev@gmail.com>
2019-06-20 17:57:18 +0300

    Removed commented code in App layer unit test

--- test/connectApp.cpp
+++ test/connectApp.cpp
@@ -4,9 +4,9 @@ int main(int argc, char *argv[]) {
 
     CPolkaApi app;
 
-   // string blockHash = "0xb72a241d0a3a38268671caa3bc4ad7e88015cff341a1da0f1982227c5a52a3e3";
-   // GetRuntimeVersionParams par;
-   // par.
+    // string blockHash = "0xb72a241d0a3a38268671caa3bc4ad7e88015cff341a1da0f1982227c5a52a3e3";
+    // GetRuntimeVersionParams par;
+    // par.
 
     unique_ptr<GetBlockHashParams> par(new GetBlockHashParams);
     par->blockNumber = 2;
@@ -16,9 +16,10 @@ int main(int argc, char *argv[]) {
     cout << "Message received(getBlockHash - console app) " << resp->hash << endl;
 
     auto resp2 = app.getSystemInfo();
-    cout << "Message received(getBlockHash - console app) " << 
-        resp2->chainId << endl << resp2->chainName << endl << 
-        resp2->tokenDecimals << endl << resp2->tokenSymbol << endl;
+    cout << "Message received(getBlockHash - console app) " << resp2->chainId << endl
+         << resp2->chainName << endl
+         << resp2->tokenDecimals << endl
+         << resp2->tokenSymbol << endl;
 
     unique_ptr<GetRuntimeVersionParams> par2(new GetRuntimeVersionParams);
     strcpy(par2->blockHash, resp->hash);
@@ -26,26 +27,11 @@ int main(int argc, char *argv[]) {
     auto resp3 = app.getRuntimeVersion(move(par2));
 
     cout << resp3->specName << endl;
-    for(auto item :resp3->api)
-    {
+    for (auto item : resp3->api) {
         cout << "num:      " << item.num << "id:      " << item.id << endl;
     }
 
     app.disconnect();
-
-
-    // JsonRpcParams params;
-    // params.jsonrpcVersion = "2.0";
-    // LoggerStub log;
-
-    // Json query = Json::object{{"method", "chain_getRuntimeVersion"}, {"params", Json::array{}}};
-
-    // CWebSocketClient::getInstance(&log)->connect();
-    // CJsonRpc jr(CWebSocketClient::getInstance(&log), &log, params);
-    // Json response = jr.request(query);
-
-    // cout << endl << "Message received(console app) " << response.dump() << endl;
-
 
     cout << "success" << endl;
 
