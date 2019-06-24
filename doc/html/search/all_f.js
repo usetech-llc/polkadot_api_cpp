e9e69a1609b37db39697dd2c3ecd3028bc9f34fc
Greg Zaitsev <greg.zaitsev@gmail.com>
2019-06-24 10:59:52 +0300

    Documentation files cleaned up

--- doc/html/search/all_f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['unsubscribews',['unsubscribeWs',['../classIJsonRpc.html#ad7fd8ba32c5d68034a1b71aa6ce870f9',1,'IJsonRpc::unsubscribeWs()'],['../classCJsonRpc.html#adfc37cfcd7229dcae5e6cc831f99fb71',1,'CJsonRpc::unsubscribeWs()']]]
-];
--- doc/html/search/classes_d.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['statics',['Statics',['../structjson11_1_1Statics.html',1,'json11']]],
-  ['storage',['Storage',['../structStorage.html',1,'']]],
-  ['systeminfo',['SystemInfo',['../structSystemInfo.html',1,'']]]
-];
