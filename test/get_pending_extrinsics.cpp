e5dd0f72cda91f14c6b91e14a3d05a5ba2cd6717
strmv <strmax20@gmail.com>
2019-07-29 09:52:19 +0400

    DOT-59 pending extrinsic
    Signed-off-by: strmv <strmax20@gmail.com>

---
+++ test/get_pending_extrinsics.cpp
@@ -0,0 +1,25 @@
+#include "../src/polkadot.h"
+#include "helpers/mockjsonrpc.h"
+#undef NDEBUG
+#include <cassert>
+
+int main(int argc, char *argv[]) {
+
+    auto app = polkadot::api::getInstance()->app();
+    app->connect();
+
+    cout << endl << endl << "============================ Get Keys ============================" << endl;
+    auto pe = app->pendingExtrinsics();
+
+    // cout << "Storage key for prefix \"" << module << " " << variable << "\" for address " << address << " : "
+    //      << actualKey << endl;
+
+    // // Check
+    // assert(expectedKey == actualKey);
+    // cout << "Matches expected key" << endl;
+
+    app->disconnect();
+    cout << "success" << endl;
+
+    return 0;
+}
